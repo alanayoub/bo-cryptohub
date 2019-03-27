5a9361e5f498522f265b773cef3d58d2057e95eb
alanayoub <alanayoub@gmail.com>
2019-03-27 14:03:52 +0000

    Update start-dev.sh

--- start-dev.sh
+++ start-dev.sh
@@ -3,6 +3,10 @@ source ~/.profile
 nvm install 9.11.1
 nvm alias default 9.11.1
 
+chmod +x build-bo-datatable.sh 
+chmod +x build-bo-utils.sh 
+chmod +x build-bo-cryptohub.sh 
+
 ./build-bo-utils.sh
 ./build-bo-datatable.sh
 ./build-bo-cryptohub.sh

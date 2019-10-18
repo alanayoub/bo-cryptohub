f0aa08ddbdfd1d613de7be2b4b12c32e65c17f3a
Alan Ayoub <alanayoub@gmail.com>
2019-10-18 13:33:31 +0100

    Turning off coinmarketcap because of fees

--- public/javascript/columns/index.js
+++ public/javascript/columns/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 import binaryoverdose from './binaryoverdose.js';
-import coinmarketcap  from './coinmarketcap.js';
+// import coinmarketcap  from './coinmarketcap.js';
 import cryptocompare  from './cryptocompare.js';
 import messari        from './messari.js';
 
 const columnDefs = {
   binaryoverdose,
-  coinmarketcap,
+  // coinmarketcap,
   cryptocompare,
   messari,
 };

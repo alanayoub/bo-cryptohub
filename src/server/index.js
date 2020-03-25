b121924843af718883909d66e6bfa1fa637ff1d7
Alan Ayoub <alanayoub@gmail.com>
2020-03-25 09:59:38 +0000

    adding server updates

--- src/server/index.js
+++ src/server/index.js
@@ -37,22 +37,22 @@ global.io = io;
 
 export default async function startServer(config) {
 
-  function shutDown() {
+  process.on('SIGTERM', shutDown);
+  process.on('SIGINT', shutDown);
+  process.on('unhandledRejection', error => {
+    logger.error(`index.js unhandledRejection: ${error}`);
+  });
 
-    console.log('Received kill signal, shutting down gracefully');
-    server.close(() => {
-      console.log('Closed out remaining connections');
-      process.exit(0);
-    });
-
-    setTimeout(() => {
-      console.error('Could not close connections in time, forcefully shutting down');
-      process.exit(1);
-    }, 10000);
-
-    connections.forEach(curr => curr.end());
-    setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
-
+  //
+  // Create folders
+  // TODO: create these on demand instead of here
+  //
+  const eventsList = Object.keys(config.events);
+  const folderTmpGenerated = join(config.cacheDir, 'tmp-generated');
+  const folderOutput = join(config.cacheDir, 'out');
+  for (const event of eventsList) {
+    await mkdirp(join(folderTmpGenerated, event));
+    await mkdirp(join(folderOutput, event));
   }
 
   const sess = {
@@ -98,31 +98,31 @@ export default async function startServer(config) {
   passprt(passport);
   routes(app);
 
-  //
-  // Setup server & socket
-  //
   logger.info('index.js: Starting server');
 
   let connections = [];
   server.on('connection', connection => {
     connections.push(connection);
-    connection.on('close', () => connections = connections.filter(curr => curr !== connection));
+    connection.on('close', () => {
+      connections = connections.filter(curr => curr !== connection);
+    });
   });
 
   server.listen(config.server.port, () => {
     logger.info(`index.js: listening on *: ${config.server.port}`);
   });
 
-  process.on('SIGTERM', shutDown);
-  process.on('SIGINT', shutDown);
+  app.get('/privacy', respondPrivacy);
+  app.get('/', respondDefault);
 
-  app.get('/privacy', (req, res, next) => {
+  io.on('connection', socketConnection);
+
+  function respondPrivacy(req, res, next) {
     const privacy = join(__dirname, '../../dist/public/privacy.html');
     res.sendFile(privacy);
-  });
+  }
 
-  app.get('/', (req, res, next) => {
-
+  function respondDefault(req, res, next) {
     const sId = req.session.id;
     const aId = req.cookies.aId;
     const lastIpAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -155,16 +155,10 @@ export default async function startServer(config) {
     }
 
     res.sendFile(config.server.index);
-
-  });
+  }
 
   let socket;
-
-  //
-  // Everytime a user connects
-  //
-  io.on('connection', async sock => {
-
+  async function socketConnection(sock) {
     logger.info(`User connected: ${sock.id}`);
 
     socket = sock;
@@ -195,20 +189,24 @@ export default async function startServer(config) {
       }
     }
 
-  });
+  }
 
-  const eventsList = Object.keys(config.events);
-  let event;
+  function shutDown() {
 
-  //
-  // Create folders
-  // NOTE: still have lots of other folders to create
-  //
-  const folderTmpGenerated = join(config.cacheDir, 'tmp-generated');
-  const folderOutput = join(config.cacheDir, 'out');
-  for (event of eventsList) {
-    await mkdirp(join(folderTmpGenerated, event));
-    await mkdirp(join(folderOutput, event));
+    console.log('Received kill signal, shutting down gracefully');
+    server.close(() => {
+      console.log('Closed out remaining connections');
+      process.exit(0);
+    });
+
+    setTimeout(() => {
+      console.error('Could not close connections in time, forcefully shutting down');
+      process.exit(1);
+    }, 10000);
+
+    connections.forEach(curr => curr.end());
+    setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
+
   }
 
 }
